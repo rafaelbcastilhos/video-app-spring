4a56629388aa9cbbe2f6a56f19cdc34697024334
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2022-01-12 07:39:10 -0300

    add application and model

---
+++ src/main/java/model/TagsModel.java
@@ -0,0 +1,22 @@
+package model;
+
+public class TagsModel {
+    private String name;
+    private String description;
+
+    public String getDesc() {
+        return this.description ;
+    }
+
+    public void setDesc(String description){
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name ;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
